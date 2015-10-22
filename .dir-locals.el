2e456b4c6b0b8b95f256b29e87c908c2414661e3
Giuseppe Scrivano <gscrivan@redhat.com>
2015-10-22 10:15:51 +0200

    .dir-locals.el: new file

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+(
+ (c-mode . (
+            (c-file-style . "K&R")
+            (indent-tabs-mode . t)
+            (c-indent-level . 8)
+            (c-basic-offset . 8)
+            (tab-width . 8)
+            ))
+ )
