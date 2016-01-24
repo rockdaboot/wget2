c1bcbcc623c262a5a930ee93c4423ecb1fc6c88c
Tim Rühsen <tim.ruehsen@gmx.de>
2016-01-24 20:04:21 +0100

    Disable valgrind tests on Travis CI for Mac OS X
    * .travis.yml: Do not install valgrind in OS X
    * .travis.sh: Disable valgrind tests for OS X

--- .travis.sh
+++ .travis.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 ./autogen.sh || exit 1
-./configure --enable-valgrind-tests || exit 1
+./configure || exit 1
 make -j3 || exit 1
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   make install || exit 1
+else
+  export VALGRIND_TESTS=1
 fi
-export VALGRIND_TESTS=1
 make check -j3
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  cat tests/test.log
+#if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+# cat tests/test.log
 # ls -la tests/.test_*
 # for log in tests/*.log; do
 #   echo -e "\n#### $log ####"
 #   cat $log
 # done
-fi
+#fi
 make distcheck
