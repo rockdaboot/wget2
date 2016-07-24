44371717113e6ff54d77d5a02eefc34759586aa0
Darshit Shah <darnir@gmail.com>
2016-07-24 17:53:20 +0200

    Document some progress bar functions

--- libwget/bar.c
+++ libwget/bar.c
@@ -40,6 +40,15 @@
 #include <libwget.h>
 #include "private.h"
 
+/**
+ * \file
+ * \brief Progress Bar Routines
+ * \defgroup libwget-progress Progress Display Functions
+ * @{
+ *
+ * Methods for creating and printing a progress bar display.
+ */
+
 typedef struct {
 	double
 		ratio;
@@ -64,17 +73,71 @@ struct _wget_bar_st {
 		allocated : 1;
 };
 
-#define _BAR_FILENAME_SIZE 20
-#define _BAR_RATIO_SIZE 4
-#define _BAR_METER_COST 2
-#define _BAR_DOWNBYTES_SIZE 8
-#define _BAR_N_ELEMENTS 4
-#define _BAR_TOTAL_SIZE (_BAR_FILENAME_SIZE + _BAR_RATIO_SIZE + _BAR_METER_COST + _BAR_DOWNBYTES_SIZE + _BAR_N_ELEMENTS - 1)
+// Define the parameters for how the progress bar looks
+#define _BAR_FILENAME_SIZE	20
+#define _BAR_RATIO_SIZE		3
+#define _BAR_METER_COST		2
+#define _BAR_DOWNBYTES_SIZE	8
 
-#define STR_HELPER(x) #x
-#define STR(x) STR_HELPER(x)
-#define FMT_STR "%-"STR(_BAR_FILENAME_SIZE)"."STR(_BAR_FILENAME_SIZE)"s %3d%% [%.*s>%.*s] %"STR(_BAR_DOWNBYTES_SIZE)"s"
+// In each of the below elements, 1 extra byte has been added to account for
+// the padding between each element.
+//
+// _BAR_RATIO_SIZE adds another extra byte to account for the %-sign at the end
+// of the ratio.
+#define _BAR_TOTAL_SIZE (_BAR_FILENAME_SIZE  + 1 + \
+                         _BAR_RATIO_SIZE     + 2 + \
+                         _BAR_METER_COST     + 1 + \
+                         _BAR_DOWNBYTES_SIZE       \
+                        )
 
+// These are helper macros to allow us to create a "string literal" dynamically
+// at compile time. printf() does not like using a dynamically generated string
+// as its format string, so we must work around that and generate it at compile
+// time.
+//
+// _STR_HELPER(x) simply prints out the argument as a string
+// _STR(x) uses _STR_HELPER(x) to print out its argument. This level of
+// indirection allows us to expand macros. This is used to generate the format
+// stirng using sizes for each component as defined above.
+#define _STR_HELPER(x) #x
+#define _STR(x) _STR_HELPER(x)
+
+// Define the format string for the progress bar. See above for an explanation
+// of why we do this in preprocessor macros.
+//
+// The progress bar looks like this:
+//
+// filename   xxx% [======>      ] xxx.xxK
+//
+// It is made of the following elements:
+// filename		_BAR_FILENAME_SIZE		Name of local file
+// xxx%			_BAR_RATIO_SIZE + 1		Amount of file downloaded
+// []			_BAR_METER_COST			Bar Decorations
+// xxx.xxK		_BAR_DOWNBYTES_SIZE		Number of downloaded bytes
+// ===>			Remaining				Progress Meter
+//
+// The final format string after the preprocessor magic should be:
+// %-20.20s %3d%% [%.*s>%.*s] %8s
+#define _FMT_STR "%-"_STR(_BAR_FILENAME_SIZE)"."_STR(_BAR_FILENAME_SIZE)"s %"_STR(_BAR_RATIO_SIZE)"d%% [%.*s>%.*s] %"_STR(_BAR_DOWNBYTES_SIZE)"s"
+
+
+/**
+ * \param[in] bar Pointer to a \p wget_bar_t object
+ * \param[in] nslots Number of progress bars
+ * \param[in] max_width Maximum width of the progress bars
+ * \return Pointer to a \p wget_bar_t object
+ *
+ * Initialize a new progress bar instance for Wget. If \p bar is a NULL
+ * pointer, it will be allocated on the heap and a pointer to the newly
+ * allocated memory will be returned.
+ *
+ * \p nslots is the number of screen lines to reserve for printing the progress
+ * bars. This may be any number, but you generally want atleast as many slots
+ * as there are downloader threads.
+ *
+ * \p max_width is the maximum number of screen columns that the progress bar
+ * may occupy.
+ */
 wget_bar_t *wget_bar_init(wget_bar_t *bar, int nslots, int max_width)
 {
 	int allocated = 0, it;
@@ -125,6 +188,11 @@ cleanup:
 	return NULL;
 }
 
+/**
+ * \param[in] bar Pointer to \p wget_bar_t
+ *
+ * Free the various progress bar data structures
+ */
 void wget_bar_deinit(wget_bar_t *bar)
 {
 	if (bar) {
@@ -134,6 +202,9 @@ void wget_bar_deinit(wget_bar_t *bar)
 	}
 }
 
+/**
+ * Free the pointer holding the \p *wget_bar_t structure as well
+ */
 void wget_bar_free(wget_bar_t **bar)
 {
 	if (bar && *bar) {
@@ -142,7 +213,16 @@ void wget_bar_free(wget_bar_t **bar)
 	}
 }
 
-
+/**
+ * \param[in] bar Pointer to \p wget_bar_t struct
+ * \param[in] slotpos Slot position to update
+ * \param[in] max Maximum expected filesize
+ * \param[in] cur Currently downloaded size
+ * \param[in] filename String
+ * \return None
+ *
+ * Update the progress bar display at slot number \p slotpos
+ */
 void wget_bar_update(const wget_bar_t *bar, int slotpos, off_t max, off_t cur, const char *filename)
 {
 	_bar_slot_t *slot = &bar->slots[slotpos];
@@ -164,11 +244,12 @@ void wget_bar_update(const wget_bar_t *bar, int slotpos, off_t max, off_t cur, c
 
 //		printf("col=%d bar->max_width=%d\n",cols,bar->max_width);
 		printf("\033[s\033[%dA\033[1G", bar->nslots - slotpos);
-		printf(FMT_STR, filename, (int) (ratio * 100), cols - 1, bar->filled, bar->max_width - cols, bar->spaces, wget_human_readable(cur, 1000, 2));
+		printf(_FMT_STR, filename, (int) (ratio * 100), cols - 1, bar->filled, bar->max_width - cols, bar->spaces, wget_human_readable(cur, 1000, 2));
 		printf("\033[u");
 		fflush(stdout);
 	}
 }
+
 
 void wget_bar_print(wget_bar_t *bar, int slotpos, const char *s)
 {
@@ -197,5 +278,6 @@ ssize_t wget_bar_printf(wget_bar_t *bar, size_t slotpos, const char *fmt, ...)
 	return len;
 }
 
-#undef STR
-#undef STR_HELPER
+#undef _STR
+#undef _STR_HELPER
+#undef _FMT_STR
