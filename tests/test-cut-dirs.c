320ac40487f65618e0cd0f0d607ff900c7ecc65a
Michael Heerklotz <michael.heerklotz@check24.de>
2017-04-12 01:55:52 +0200

    Fix --cut-dirs
    * src/wget.c (get_local_filename): Add / after host part
    * tests/Makefile.am: Add new test 'test-cut-dirs'
    * tests/test-cut-dirs.c: New file

---
+++ tests/test-cut-dirs.c
@@ -0,0 +1,77 @@
+/*
+ * Copyright(c) 2017 Free Software Foundation, Inc.
+ *
+ * This file is part of libwget.
+ *
+ * Libwget is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Libwget is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libwget.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Testing for --cut-dirs=x
+ *
+ *
+ */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdlib.h> // exit()
+#include "libtest.h"
+
+int main(void)
+{
+	wget_test_url_t urls[]={
+		{	.name = "/page1.html",
+			.code = "200 Dontcare",
+			.body =
+				"<html><head><title>Page 1</title></head><body><p>Hello 1</p></body></html>",
+			.headers = {
+				"Content-Type: text/html",
+			}
+		},
+		{	.name = "/subdir/page2.html",
+			.code = "200 Dontcare",
+			.body =
+				"<html><head><title>Page 2</title></head><body><p>Hello 2</p></body></html>",
+			.headers = {
+				"Content-Type: text/html",
+			}
+		},
+	};
+
+	// functions won't come back if an error occurs
+	wget_test_start_server(
+		WGET_TEST_RESPONSE_URLS, &urls, countof(urls),
+		0);
+
+	wget_test(
+		WGET_TEST_OPTIONS, "-r -nH --cut-dirs=1",
+		WGET_TEST_REQUEST_URL, "page1.html",
+		WGET_TEST_EXPECTED_ERROR_CODE, 0,
+		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+			{ "page1.html", urls[0].body },
+			{	NULL } },
+		0);
+
+	wget_test(
+		WGET_TEST_OPTIONS, "-r -nH --cut-dirs=1",
+		WGET_TEST_REQUEST_URL, "subdir/page2.html",
+		WGET_TEST_EXPECTED_ERROR_CODE, 0,
+		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+			{ "page2.html", urls[1].body },
+			{	NULL } },
+		0);
+		
+	exit(0);
+}
