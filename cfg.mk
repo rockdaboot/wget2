1868fa0311a98533e756101fc702296e4207e3da
Tim Rühsen <tim.ruehsen@gmx.de>
2017-05-29 12:41:03 +0200

    Enhance fuzzing
    * cfg.mk: Let make-syntax-check survive
    * fuzz/libwget_metalink_parse.dict: New dictionary
    * fuzz/libwget_robots_parse.dict: New dictionary
    * fuzz/libwget_xml_parse_buffer.dict: New dictionary
    * fuzz/libwget_robots_parse.in/disallowed1: Obsolete by afl-cmin
    * fuzz/libwget_robots_parse.in/user_agent3: Likewise
    * fuzz/libwget_xml_parse_buffer.in/index.html: Removed redundant HTML tags
    * fuzz/run-afl.sh: Auto-optimize corpora with afl-tmin

--- cfg.mk
+++ cfg.mk
@@ -1,5 +1,5 @@
 # Explicit syntax-check exceptions.
-VC_LIST_ALWAYS_EXCLUDE_REGEX = ^data/.*|^tests/files/.*|.*.png$$
+VC_LIST_ALWAYS_EXCLUDE_REGEX = ^data/.*|^tests/files/.*|.*.png|^fuzz/.*.in/.*$$
 
 local-checks-to-skip = \
   sc_cast_of_argument_to_free \
@@ -10,10 +10,10 @@ local-checks-to-skip = \
   sc_prohibit_strcmp
 
 # Explicit syntax-check exceptions.
-exclude_file_name_regexp--sc_bindtextdomain = ^(tests|examples)/.*.c|^libwget/test_linking.c$$
+exclude_file_name_regexp--sc_bindtextdomain = ^(tests|examples|fuzz)/.*.c|^libwget/test_linking.c$$
 exclude_file_name_regexp--sc_po_check = ^examples/|^tests/.*.c$$
 exclude_file_name_regexp--sc_prohibit_magic_number_exit = ^tests/.*.c$$
 exclude_file_name_regexp--sc_trailing_blank = docs/DoxygenLayout.xml|docs/libwget.doxy.in$$
 exclude_file_name_regexp--sc_two_space_separator_in_usage = docs/wget2_manual.md$$
-exclude_file_name_regexp--sc_require_config_h = examples/.*.c$$
-exclude_file_name_regexp--sc_require_config_h_first = examples/.*.c$$
+exclude_file_name_regexp--sc_require_config_h = examples/.*.c|fuzz/main.c$$
+exclude_file_name_regexp--sc_require_config_h_first = examples/.*.c|fuzz/main.c$$
--- fuzz/run-afl.sh
+++ fuzz/run-afl.sh
@@ -20,13 +20,13 @@ srcdir="${srcdir:-.}"
 export LD_LIBRARY_PATH=${srcdir}/../lib/.libs/
 
 cat ${srcdir}/../config.log|grep afl-clang-fast >/dev/null 2>&1
-if test $? != 0;then
+if test $? != 0; then
 	echo "compile first library as:"
 	echo "CC=afl-clang-fast ./configure"
 	exit 1
 fi
 
-if test -z "$1";then
+if test -z "$1"; then
 	echo "Usage: $0 test-case"
 	echo "Example: $0 libwget_robots_parse_fuzzer"
 	exit 1
@@ -37,9 +37,27 @@ CFLAGS="-g -O2" CC=afl-clang-fast make "$1" || exit 1
 
 TEST=$(echo $1|sed s/_fuzzer//)
 
+### minimize test corpora
+if test -d $TEST.in; then
+  mkdir -p $TEST.min
+  for i in `ls $TEST.in`; do
+    fin="$TEST.in/$i"
+    fmin="$TEST.min/$i"
+    echo $fin / $fmin
+    if ! test -e $fmin || $fin -nt $fmin; then
+      afl-tmin -i $fin -o $fmin -- ./${TEST}_fuzzer
+    fi
+  done
+fi
+
 TMPOUT=${TEST}.$$.out
 mkdir -p ${TMPOUT}
-afl-fuzz -i ${TEST}.in -o ${TMPOUT} -- ./${TEST}_fuzzer
+
+if test -f ${TEST}.dict; then
+  afl-fuzz -i ${TEST}.min -o ${TMPOUT} -x ${TEST}.dict -- ./${TEST}_fuzzer
+else
+  afl-fuzz -i ${TEST}.min -o ${TMPOUT} -- ./${TEST}_fuzzer
+fi
 
 echo "output was stored in $TMPOUT"
 
