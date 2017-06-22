832de1116169ca977d5b0201d847c4fdea27307a
Tim Rühsen <tim.ruehsen@gmx.de>
2017-06-22 10:28:01 +0200

    * fuzz/libwget_xml_parse_buffer_fuzzer.c: Increase fuzz code coverage

--- fuzz/libwget_xml_parse_buffer_fuzzer.c
+++ fuzz/libwget_xml_parse_buffer_fuzzer.c
@@ -53,5 +53,8 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 
 	free(in);
 
+	wget_html_parse_file("/dev/null", NULL, NULL, 0);
+	wget_html_parse_file("-", NULL, NULL, 0);
+
 	return 0;
 }
