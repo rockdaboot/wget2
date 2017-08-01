df19bcdabfc3c321fc0161605149bcbb1cc493f9
Tim Rühsen <tim.ruehsen@gmx.de>
2017-08-01 14:38:26 +0200

    Add new fuzzer libwget_hsts_fuzzer
    * fuzz/Makefile.am: Add libwget_hsts_fuzzer
    * fuzz/libwget_hsts_fuzzer.c: New file
    * fuzz/libwget_hsts_fuzzer.in/*: Add fuzz corpora

---
+++ fuzz/libwget_hsts_fuzzer.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright(c) 2017 Free Software Foundation, Inc.
+ *
+ * This file is part of libwget.
+ *
+ * Libwget is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Libwget is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libwget.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "wget.h"
+#include "fuzzer.h"
+
+static const uint8_t *g_data;
+static size_t g_size;
+
+#if ! defined _WIN32 && defined HAVE_FMEMOPEN
+#include <dlfcn.h>
+FILE *fopen(const char *pathname, const char *mode)
+{
+	FILE *(*libc_fopen)(const char *, const char *) =
+		(FILE *(*)(const char *, const char *)) dlsym (RTLD_NEXT, "fopen");
+
+	if (!strcmp(pathname, "hsts"))
+		return fmemopen((void *) g_data, g_size, mode);
+
+	return libc_fopen(pathname, mode);
+}
+#endif
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+	if (size > 256) // same as max_len = 256 in .options file
+		return 0;
+
+	g_data = data;
+	g_size = size;
+
+	wget_hsts_db_t *hsts_db = wget_hsts_db_init(NULL);
+#if ! defined _WIN32 && defined HAVE_FMEMOPEN
+	wget_hsts_db_load(hsts_db, "hsts");
+#endif
+	wget_hsts_host_match(hsts_db, "x.y", 80);
+	wget_hsts_db_free(&hsts_db);
+
+	return 0;
+}
