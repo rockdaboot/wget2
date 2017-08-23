6f742a293da9a2b82610aa90cd75e521addd1c69
Tim Rühsen <tim.ruehsen@gmx.de>
2017-08-23 21:54:14 +0200

    Improve fuzzer helper scripts
    * fuzz/get_ossfuzz_corpora: Improve description text
    * fuzz/view-coverage.sh: Allow 1+ arguments

--- fuzz/view-coverage.sh
+++ fuzz/view-coverage.sh
@@ -1,21 +1,37 @@
 #!/bin/bash -eu
+#
+# (C)2017 Tim Ruehsen tim.ruehsen@gmx.de
+#
+# View the coverage report for one or more fuzzers.
 
 # 1. execute 'make fuzz-coverage' in the top directory
-# 2. execute './view-coverage.sh <fuzz target>
-#    example: ./view-coverage.sh libwget_xml_parse_buffer_fuzzer
+# 2. execute './view-coverage.sh <fuzz target(s)>
+
+# Example with single fuzzer:
+#   ./view-coverage.sh libwget_xml_parse_buffer_fuzzer
+
+# Example with two fuzzers:
+#   ./view-coverage.sh libwget_xml_parse_buffer_fuzzer libwget_metalink_parse_fuzzer
 
 if test -z "$1"; then
-  echo "Usage: $0 <fuzz target>"
+  echo "Usage: $0 <fuzz target(s)>"
   echo "Example: $0 libwget_xml_parse_buffer_fuzzer"
   exit 1
 fi
 
-fuzzer="./"$1
+#fuzzer="./"$1
 LCOV_INFO=coverage.info
-#make fuzz-coverage CFLAGS="$(CFLAGS) --coverage" LDFLAGS="$(LDFLAGS) --coverage"
-./coverage.sh $fuzzer
+#./coverage.sh $fuzzer
+#lcov --capture --initial --directory ../libwget/.libs --directory . --output-file $LCOV_INFO
+#lcov --capture --directory ../libwget/.libs --output-file $LCOV_INFO
+#lcov --remove $LCOV_INFO '*/test_linking.c' '*/css_tokenizer.lex' '*/<stdout>' '*/*.h' -o $LCOV_INFO
+#genhtml --prefix . --ignore-errors source $LCOV_INFO --legend --title "$1" --output-directory=lcov
+
+lcov --zerocounters --directory ../libwget/
 lcov --capture --initial --directory ../libwget/.libs --directory . --output-file $LCOV_INFO
+make check TESTS="$*" CFLAGS="$(CFLAGS) --coverage" LDFLAGS="$(LDFLAGS) --coverage"
 lcov --capture --directory ../libwget/.libs --output-file $LCOV_INFO
-lcov --remove $LCOV_INFO '*/test_linking.c' '*/css_tokenizer.lex' '*/<stdout>' '*/*.h' -o $LCOV_INFO
-genhtml --prefix . --ignore-errors source $LCOV_INFO --legend --title "$1" --output-directory=lcov
+lcov --remove $LCOV_INFO '*/test_linking.c' '*/css_tokenizer.lex' '*/*.h' -o $LCOV_INFO
+genhtml --prefix . --ignore-errors source $LCOV_INFO --legend --title "$*" --output-directory=lcov
+
 xdg-open lcov/index.html
