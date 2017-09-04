927bbc55dc4f991b36ad63b009dfeefd619d88f3
Avinash Sonawane <rootkea@gmail.com>
2017-09-04 13:18:10 +0530

    * tests/test-stats.c: Add tests for -r with --stats-* options

--- tests/test-stats.c
+++ tests/test-stats.c
@@ -33,7 +33,20 @@ static const char *mainpage = "\
 </head>\n\
 <body>\n\
   <p>\n\
-    Don't care.\n\
+    <a href=\"http://localhost:{{port}}/secondpage.html\">second page</a>.\n\
+    <a href=\"http://localhost:{{port}}/thirdpage.html\">third page</a>.\n\
+  </p>\n\
+</body>\n\
+</html>\n";
+
+static const char *subpage = "\
+<html>\n\
+<head>\n\
+  <title>Main Page</title>\n\
+</head>\n\
+<body>\n\
+  <p>\n\
+    Some text\n\
   </p>\n\
 </body>\n\
 </html>\n";
@@ -44,6 +57,20 @@ int main(void)
 		{	.name = "/index.html", // "gnosis" in UTF-8 greek
 			.code = "200 Dontcare",
 			.body = mainpage,
+			.headers = {
+				"Content-Type: text/html",
+			}
+		},
+		{	.name = "/secondpage.html",
+			.code = "200 Dontcare",
+			.body = subpage,
+			.headers = {
+				"Content-Type: text/html",
+				}
+		},
+		{	.name = "/thirdpage.html",
+			.code = "200 Dontcare",
+			.body = subpage,
 			.headers = {
 				"Content-Type: text/html",
 			}
@@ -108,6 +135,51 @@ int main(void)
 					{	NULL } },
 				0);
 		}
+
+		// test stats option without additional params With -r
+		snprintf(options, sizeof(options), "%s -r -nH", stats_options[it]);
+		wget_test(
+			// WGET_TEST_KEEP_TMPFILES, 1,
+			WGET_TEST_OPTIONS, options,
+			WGET_TEST_REQUEST_URL, urls[0].name + 1,
+			WGET_TEST_EXPECTED_ERROR_CODE, 0,
+			WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+				{ "index.html", urls[0].body },
+				{ "secondpage.html", urls[1].body },
+				{ "thirdpage.html", urls[2].body },
+				{	NULL } },
+			0);
+
+		// test stats option without format With -r
+		snprintf(options, sizeof(options), "%s=- -r -nH", stats_options[it]);
+		wget_test(
+			// WGET_TEST_KEEP_TMPFILES, 1,
+			WGET_TEST_OPTIONS, options,
+			WGET_TEST_REQUEST_URL, urls[0].name + 1,
+			WGET_TEST_EXPECTED_ERROR_CODE, 0,
+			WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+				{ "index.html", urls[0].body },
+				{ "secondpage.html", urls[1].body },
+				{ "thirdpage.html", urls[2].body },
+				{	NULL } },
+			0);
+
+		for (unsigned it2 = 0; it2 < countof(stats_format); it2++) {
+			// test stats option with format With -r
+			snprintf(options, sizeof(options), "%s=%s:- -r -nH", stats_options[it], stats_format[it2]);
+			wget_test(
+				// WGET_TEST_KEEP_TMPFILES, 1,
+				WGET_TEST_OPTIONS, options,
+				WGET_TEST_REQUEST_URL, urls[0].name + 1,
+				WGET_TEST_EXPECTED_ERROR_CODE, 0,
+				WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+					{ "index.html", urls[0].body },
+					{ "secondpage.html", urls[1].body },
+					{ "thirdpage.html", urls[2].body },
+					{	NULL } },
+				0);
+		}
+
 	}
 
 	exit(0);
