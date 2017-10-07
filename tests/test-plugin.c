93f88709be8800f8b1ab69b158693288c91c6457
Tim Rühsen <tim.ruehsen@gmx.de>
2017-10-07 20:47:00 +0200

    Add --no-local-db to test suite
    * tests/libtest.c: Add --no-local-db to wget2 options
    * tests/test-plugin.c: Fix test

--- tests/test-plugin.c
+++ tests/test-plugin.c
@@ -690,12 +690,24 @@ int main(void)
 
 	// Check whether priority based database selection works correctly
 	wget_test(
-		WGET_TEST_OPTIONS, "--local-plugin=" LOCAL_NAME("plugindb"),
+		WGET_TEST_OPTIONS,
+			"--hpkp --hpkp-file=hpkp.db "
+			"--hsts --hsts-file=hsts.db "
+			"--ocsp --ocsp-file=ocsp.db "
+			"--local-plugin=" LOCAL_NAME("plugindb"),
 		WGET_TEST_REQUEST_URL, "index.html",
 		WGET_TEST_EXPECTED_ERROR_CODE, 0,
+		WGET_TEST_EXISTING_FILES, &(wget_test_file_t []) {
+			{ "hpkp.db", "# HPKP 1.0 file\nexample.com 1491338542 1 1488746542\n*sha256 8dNiZZueNzmyaf3pTkXxDgOzLkjKvI+Nza0ACF5IDwg=" },
+			{ "hsts.db", "#HSTS 1.0 file\nexample.com 443 1 1499023633 63072000" },
+			{ "ocsp.db", "#OCSP 1.0 file\n" },
+			{ NULL } },
 		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
 			{ "index.html", urls[0].body },
-			{	NULL } },
+			{ "hpkp.db", NULL },
+			{ "hsts.db", NULL },
+			{ "ocsp.db", NULL },
+			{ NULL } },
 		0);
 
 	exit(0);
