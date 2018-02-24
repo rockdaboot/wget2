2f00c204050715c2ca5be0b21899f33495897af3
Tim Rühsen <tim.ruehsen@gmx.de>
2018-02-24 15:22:45 +0100

    * tests/test-gzip.c: Add tests for broken server gzip Content-Encoding

--- tests/test-gzip.c
+++ tests/test-gzip.c
@@ -22,23 +22,46 @@
 #include <stdlib.h> // exit()
 #include "libtest.h"
 
+// gzipp'ed content 'x'
+#define compressed_body "\x1f\x8b\x08\x08\x48\x5d\x91\x5a\x00\x03\x78\x00\xab\x00\x00\x83\x16\xdc\x8c\x01\x00\x00\x00"
+#define uncompressed_body "x"
+
 int main(void)
 {
 	wget_test_url_t urls[]={
 		{	.name = "/index.html",
 			.code = "200 Dontcare",
-			.body = "from HTTPS",
+			.body = compressed_body,
+			.body_len = sizeof(compressed_body) - 1,
 			.headers = {
 				"Content-Type: application/gzip",
 				"Content-Encoding: gzip",
 			},
 		},
-		// gzipp'ed 0 byte file
 		{	.name = "/index2.html",
 			.code = "200 Dontcare",
-			.body = "\x1f\x8b\x08\x08\xee\x86\x90\x5a\x00\x03\x78\x00\x03\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+			.body = compressed_body,
+			.body_len = sizeof(compressed_body) - 1,
 			.headers = {
 				"Content-Type: text/html",
+				"Content-Encoding: gzip",
+			},
+		},
+		{	.name = "/ball.tgz",
+			.code = "200 Dontcare",
+			.body = compressed_body,
+			.body_len = sizeof(compressed_body) - 1,
+			.headers = {
+				"Content-Type: application/x-tar",
+				"Content-Encoding: gzip",
+			},
+		},
+		{	.name = "/ball.tar.gz",
+			.code = "200 Dontcare",
+			.body = compressed_body,
+			.body_len = sizeof(compressed_body) - 1,
+			.headers = {
+				"Content-Type: application/x-tar",
 				"Content-Encoding: gzip",
 			},
 		},
@@ -51,17 +74,21 @@ int main(void)
 		0);
 
 	// wget2 should not decompress application/gzip
-/*	wget_test(
+	wget_test(
 		// WGET_TEST_KEEP_TMPFILES, 1,
 		WGET_TEST_OPTIONS, "",
 		WGET_TEST_REQUEST_URL, urls[0].name + 1,
 		WGET_TEST_EXPECTED_ERROR_CODE, 0,
 		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
-			{ urls[0].name + 1, urls[0].body },
+#ifdef WITH_ZLIB
+			{ urls[0].name + 1, compressed_body, .content_length = sizeof(compressed_body) - 1 },
+#else
+			{ urls[0].name + 1, compressed_body, .content_length = sizeof(compressed_body) - 1 },
+#endif
 			{	NULL } },
 		0);
-*/
-	// wget2 should not decompress text/html
+
+	// wget2 should decompress gzipped'ed text/html
 	wget_test(
 		// WGET_TEST_KEEP_TMPFILES, 1,
 		WGET_TEST_OPTIONS, "",
@@ -69,9 +96,39 @@ int main(void)
 		WGET_TEST_EXPECTED_ERROR_CODE, 0,
 		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
 #ifdef WITH_ZLIB
-			{ urls[1].name + 1, "" },
+			{ urls[1].name + 1, uncompressed_body, .content_length = sizeof(uncompressed_body) - 1 },
 #else
-			{ urls[1].name + 1, urls[1].body },
+			{ urls[1].name + 1, compressed_body, .content_length = sizeof(compressed_body) - 1 },
+#endif
+			{	NULL } },
+		0);
+
+	// wget2 should *not* decompress if name ends with .tgz
+	wget_test(
+		// WGET_TEST_KEEP_TMPFILES, 1,
+		WGET_TEST_OPTIONS, "",
+		WGET_TEST_REQUEST_URL, urls[2].name + 1,
+		WGET_TEST_EXPECTED_ERROR_CODE, 0,
+		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+#ifdef WITH_ZLIB
+			{ urls[2].name + 1, compressed_body, .content_length = sizeof(compressed_body) - 1 },
+#else
+			{ urls[2].name + 1, compressed_body, .content_length = sizeof(compressed_body) - 1 },
+#endif
+			{	NULL } },
+		0);
+
+	// wget2 should *not* decompress if name ends with .gz
+	wget_test(
+		// WGET_TEST_KEEP_TMPFILES, 1,
+		WGET_TEST_OPTIONS, "",
+		WGET_TEST_REQUEST_URL, urls[3].name + 1,
+		WGET_TEST_EXPECTED_ERROR_CODE, 0,
+		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+#ifdef WITH_ZLIB
+			{ urls[3].name + 1, compressed_body, .content_length = sizeof(compressed_body) - 1 },
+#else
+			{ urls[3].name + 1, compressed_body, .content_length = sizeof(compressed_body) - 1 },
 #endif
 			{	NULL } },
 		0);
