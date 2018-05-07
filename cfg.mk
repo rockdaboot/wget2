623c59b88a320900051237eeb5cd61a32fe00081
Tim Rühsen <tim.ruehsen@gmx.de>
2018-05-07 12:30:31 +0200

    * cfg.mk: Escape dots in path components

--- cfg.mk
+++ cfg.mk
@@ -1,5 +1,5 @@
 # Explicit syntax-check exceptions.
-VC_LIST_ALWAYS_EXCLUDE_REGEX = ^data/.*|tests/(files|gpg|certs)/.*|.*.png|^fuzz/.*.in/.*|.*.repro/.*$$
+VC_LIST_ALWAYS_EXCLUDE_REGEX = ^data/.*|tests/(files|gpg|certs)/.*|.*\.png|^fuzz/.*\.(in|repro)/.*$$
 
 local-checks-to-skip = \
   sc_cast_of_argument_to_free \
@@ -10,12 +10,12 @@ local-checks-to-skip = \
   sc_prohibit_strcmp
 
 # Explicit syntax-check exceptions.
-exclude_file_name_regexp--sc_bindtextdomain = ^(tests|unit-tests|examples|fuzz)/.*.c|^libwget/test_linking.c$$
-exclude_file_name_regexp--sc_po_check = ^examples/|tests/.*.c$$
-exclude_file_name_regexp--sc_prohibit_magic_number_exit = tests/.*.c$$
-exclude_file_name_regexp--sc_trailing_blank = docs/DoxygenLayout.xml|docs/libwget.doxy.in|contrib/assignment_template.txt$$
-exclude_file_name_regexp--sc_two_space_separator_in_usage = .gitlab-ci.yml|docs/wget2_manual.md$$
-exclude_file_name_regexp--sc_require_config_h = examples/.*.c|fuzz/main.c$$
-exclude_file_name_regexp--sc_require_config_h_first = examples/.*.c|fuzz/main.c$$
-exclude_file_name_regexp--sc_copyright_check = .*gnulib/.*.c$$
-exclude_file_name_regexp--sc_prohibit_empty_lines_at_EOF = contrib/assignment_template.txt$$
+exclude_file_name_regexp--sc_bindtextdomain = ^(tests|unit-tests|examples|fuzz)/.*\.c|^libwget/test_linking\.c$$
+exclude_file_name_regexp--sc_po_check = ^examples/|tests/.*\.c$$
+exclude_file_name_regexp--sc_prohibit_magic_number_exit = tests/.*\.c$$
+exclude_file_name_regexp--sc_trailing_blank = docs/DoxygenLayout\.xml|docs/libwget\.doxy\.in|contrib/assignment_template\.txt$$
+exclude_file_name_regexp--sc_two_space_separator_in_usage = \.gitlab-ci\.yml|docs/wget2_manual\.md$$
+exclude_file_name_regexp--sc_require_config_h = examples/.*\.c|fuzz/main\.c$$
+exclude_file_name_regexp--sc_require_config_h_first = examples/.*\.c|fuzz/main\.c$$
+exclude_file_name_regexp--sc_copyright_check = .*gnulib/.*\.c$$
+exclude_file_name_regexp--sc_prohibit_empty_lines_at_EOF = contrib/assignment_template\.txt$$
