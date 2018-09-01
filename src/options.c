rockdaboot/wget2

commit 756e470de942ec830a9a424a2340b7e986e9fe7c

Add -X/--exclude-directories and -I/--include-directories
* docs/wget2.md: Add docs for -I/--include-directories=list
  and -X/--exclude-directories=list
* src/options.c: New function _strchrnul_esc(),
  new function _strmemdup_esc(),
  (parse_stringlist_expand): Use new functions from above,
  new function set_char_prefix(), parse_included_directories()
  and parse_excluded_directories,
  (options): add exclude-directories and --include-directories
* src/wget.c: New functions match_subdir() and in_directory_pattern_list(),
  (add_url): check if path matches directory patterns,
  (_prepare_file): add new param 'path',
* src/wget_options.h (struct config): Add member 'exclude_directories'
* tests/Makefile.am: Add 'test-include-and-exclude-directories'
* tests/test-include-and-exclude-directories.c: New file

src/options.c
@@ -383,6 +383,50 @@ static int parse_header(option_t opt, const char *val, G_GNUC_WGET_UNUSED const 
 	return 0;
 }
 
+static const char *_strchrnul_esc(const char *s, char c)
+{
+	const char *p;
+
+	for (p = s; *p; p++) {
+		if (*p == '\\' && (p[1] == '\\' || p[1] == c))
+			p++;
+		else if (*p == c)
+			return p;
+	}
+
+	return p; // pointer to trailing \0
+}
+
+static char *_strmemdup_esc(const char *s, size_t size)
+{
+   const char *p, *e;
+	size_t newsize = 0;
+
+	for (p = s, e = s + size; p < e; p++) {
+		if (*p == '\\') {
+			if (p < e - 1) {
+				newsize++;
+				p++;
+			}
+		} else
+			newsize++;
+	}
+
+	char *ret = wget_malloc(newsize + 1);
+	char *dst = ret;
+
+	for (p = s, e = s + size; p < e; p++) {
+		if (*p == '\\') {
+			if (p < e - 1)
+				*dst++ = *++p;
+		} else
+			*dst++ = *p;
+	}
+	*dst = 0;
+
+	return ret;
+}
+
 static int parse_stringlist_expand(option_t opt, const char *val, int expand, int max_entries)
 {
 	if (val && *val) {
@@ -393,13 +437,13 @@ static int parse_stringlist_expand(option_t opt, const char *val, int expand, in
 			v = *((wget_vector_t **)opt->var) = wget_vector_create(8, (wget_vector_compare_t)strcmp);
 
 		for (s = p = val; *p; s = p + 1) {
-			if ((p = strchrnul(s, ',')) != s) {
+			if ((p = _strchrnul_esc(s, ',')) != s) {
 				if (wget_vector_size(v) >= max_entries) {
 					wget_debug_printf("%s: More than %d entries, ignoring overflow\n", __func__, max_entries);
 					return -1;
 				}
 
-				const char *fname = wget_strmemdup(s, p - s);
+				const char *fname = _strmemdup_esc(s, p - s);
 
 				if (expand && *s == '~') {
 					wget_vector_add_noalloc(v, shell_expand(fname));
@@ -419,6 +463,60 @@ static int parse_stringlist(option_t opt, const char *val, G_GNUC_WGET_UNUSED co
 {
 	/* max number of 1024 entries to avoid out-of-memory */
 	return parse_stringlist_expand(opt, val, 0, 1024);
+}
+
+static char *set_char_prefix(const char *val, char prefix)
+{
+	if (val && *val) {
+		// we just need a scratch buffer, no need for optimal size calculation
+		char *prefixed_val = wget_malloc(strlen(val) * 2 + 1), *dst = prefixed_val;
+
+		*dst++ = prefix;
+		for (const char *src = val; *src; src++) {
+			if (*src == '\\') {
+				*dst++ = *src++;
+				if (*src)
+					*dst++ = *src;
+			} else if (*src == ',') {
+				while (dst[-1] == '/')
+					dst--;
+				*dst++ = *src;
+				*dst++ = prefix;
+			} else
+				*dst++ = *src;
+		}
+
+		while (dst[-1] == '/')
+			dst--;
+
+		*dst = 0;
+
+		return prefixed_val;
+	}
+
+	return NULL;
+}
+
+static int parse_included_directories(option_t opt, const char *val, G_GNUC_WGET_UNUSED const char invert)
+{
+	char *prefixed_val = set_char_prefix(val, INCLUDED_DIRECTORY_PREFIX);
+	int ret = parse_stringlist_expand(opt, prefixed_val, 0, 1024);
+
+	if (prefixed_val)
+		xfree(prefixed_val);
+
+	return ret;
+}
+
+static int parse_excluded_directories(option_t opt, const char *val, G_GNUC_WGET_UNUSED const char invert)
+{
+	char *prefixed_val = set_char_prefix(val, EXCLUDED_DIRECTORY_PREFIX);
+	int ret = parse_stringlist_expand(opt, prefixed_val, 0, 1024);
+
+	if (prefixed_val)
+		xfree(prefixed_val);
+
+	return ret;
 }
 
 static int parse_filenames(option_t opt, const char *val, G_GNUC_WGET_UNUSED const char invert)
@@ -1276,6 +1374,12 @@ static const struct optionw options[] = {
 		  "Entropy Gathering Daemon.\n"
 		}
 	},
+	{ "exclude-directories", &config.exclude_directories, parse_excluded_directories, 1, 'X',
+		SECTION_DOWNLOAD,
+		{ "Comma-separated list of directories NOT to download.\n",
+		  "Wildcards are allowed.\n"
+		}
+	},
 	{ "exclude-domains", &config.exclude_domains, parse_stringlist, 1, 0,
 		SECTION_DOWNLOAD,
 		{ "Comma-separated list of domains NOT to follow.\n"
@@ -1294,7 +1398,7 @@ static const struct optionw options[] = {
 	{ "filter-urls", &config.filter_urls, parse_bool, 0, 0,
 		SECTION_DOWNLOAD,
 		{ "Apply the accept and reject filters on the URL\n",
-                  "before starting a download. (default: off)\n"
+		  "before starting a download. (default: off)\n"
 		}
 	},
 	{ "follow-tags", &config.follow_tags, parse_taglist, 1, 0,
@@ -1478,6 +1582,12 @@ static const struct optionw options[] = {
 		SECTION_DOWNLOAD,
 		{ "Ignore tag/attributes for URL scanning,\n",
 		  "e.g. --ignore-tags=\"img,a/href\n"
+		}
+	},
+	{ "include-directories", &config.exclude_directories, parse_included_directories, 1, 'I',
+		SECTION_DOWNLOAD,
+		{ "Comma-separated list of directories TO download.\n",
+		  "Wildcards are allowed.\n"
 		}
 	},
 	{ "inet4-only", &config.inet4_only, parse_bool, -1, '4',
@@ -2727,7 +2837,6 @@ int init(int argc, const char **argv)
 	config.secure_protocol = wget_strdup(config.secure_protocol);
 	config.ca_directory = wget_strdup(config.ca_directory);
 	config.default_page = wget_strdup(config.default_page);
-	config.domains = wget_vector_create(16, (wget_vector_compare_t)strcmp);
 
 	// create list of default config file names
 	const char *env;
@@ -2784,6 +2893,7 @@ int init(int argc, const char **argv)
 		config.netrc_file = wget_aprintf("%s/.netrc", home_dir);
 
 	xfree(home_dir);
+	wget_vector_free(&config.exclude_directories); // -I and -X stack up, so free before final command line parsing
 
 	//Enable plugin loading
 	{
@@ -3151,6 +3261,7 @@ void deinit(void)
 
 	wget_iri_free(&config.base);
 
+	wget_vector_free(&config.exclude_directories);
 	wget_vector_free(&config.mime_types);
 	wget_vector_free(&config.http_retry_on_status);
 	wget_vector_free(&config.domains);

src/wget.c
@@ -124,7 +124,7 @@ static _statistics_t stats;
 
 static int G_GNUC_WGET_NONNULL((1)) _prepare_file(wget_http_response_t *resp, const char *fname, int flag,
 		const char *uri, const char *original_url, int ignore_patterns, wget_buffer_t *partial_content,
-		size_t max_partial_content, char **actual_file_name);
+		size_t max_partial_content, char **actual_file_name, const char *path);
 
 static void
 	sitemap_parse_xml(JOB *job, const char *data, const char *encoding, wget_iri_t *base),
@@ -471,6 +471,76 @@ static void _wget_deinit(void)
 	wget_thread_mutex_destroy(&quota_mutex);
 	wget_thread_cond_destroy(&main_cond);
 	wget_thread_cond_destroy(&worker_cond);
+}
+
+/* Check if 'subdir' is a subdirectory of 'dir'.
+ * E.g. if 'dir' is `/something', match_subdir() will return true if and
+ * only if 'subdir' begins with `/something/' or is exactly '/something'.
+ */
+static bool match_subdir(const char *dir, const char *subdir, char ignore_case)
+{
+	if (*dir == '\0')
+		return (strcmp(subdir, "/")) ? false : true;
+
+	if (ignore_case)
+		for (; *dir && *subdir && (c_tolower(*dir) == c_tolower(*subdir)); ++dir, ++subdir)
+			;
+	else
+		while (*dir && *subdir && (*dir++ == *subdir++))
+			;
+
+	return *dir == 0 && (*subdir == 0 || *subdir == '/');
+}
+
+static int in_directory_pattern_list(const wget_vector_t *v, const char *fname)
+{
+	// if -I was given: exclude all be default
+	// if -X was given alone: include all be default
+	const char *pattern;
+	char *path;
+	bool default_exclude = 0;
+
+	if (*fname == '/')
+		fname++;
+
+	const char *e = strrchr(fname, '/');
+	if (!e)
+		//return default_exclude; // no path component found
+		path = wget_strdup("/");
+	else
+		path = wget_strmemdup(fname, e - fname);
+
+	pattern = wget_vector_get(v, 0);
+	default_exclude = (*pattern == INCLUDED_DIRECTORY_PREFIX);
+
+	for (int it = wget_vector_size(v) - 1; it >= 0; it--) {
+		pattern = wget_vector_get(v, it);
+
+		bool exclude = (*pattern != INCLUDED_DIRECTORY_PREFIX);
+
+		pattern++;
+
+		if (*pattern == '/')
+			pattern++;
+
+		debug_printf("directory[%d] '%s' - '%s' %c\n", it, pattern, path, "+-"[exclude]);
+
+		if (strpbrk(pattern, "*?[]")) {
+			// path="/we/all/love/wget" wouldn't match "/*/all/*" but "/*/all/*/*"
+			if (!fnmatch(pattern, path, FNM_PATHNAME | (config.ignore_case ? FNM_CASEFOLD : 0))) {
+				wget_free(path);
+				return exclude;
+			}
+		} else if (match_subdir(pattern, path, config.ignore_case)) {
+			// path="/we/all/love/wget" would match "/we/all/"
+			wget_free(path);
+			return exclude;
+		}
+	}
+
+	wget_free(path);
+
+	return default_exclude;
 }
 
 static int in_pattern_list(const wget_vector_t *v, const char *url)
@@ -915,6 +985,12 @@ static void add_url(JOB *job, const char *encoding, const char *url, int flags)
 			wget_thread_mutex_unlock(downloader_mutex);
 			return;
 		}
+
+		if (config.exclude_directories && in_directory_pattern_list(config.exclude_directories, iri->path)) {
+			debug_printf("not requesting '%s' (path excluded)\n", iri->uri);
+			wget_thread_mutex_unlock(downloader_mutex);
+			return;
+		}
 	}
 
 	new_job = job_init(&job_buf, iri, http_fallback);
@@ -969,6 +1045,9 @@ static void add_url(JOB *job, const char *encoding, const char *url, int flags)
 
 		if ((config.reject_patterns && in_pattern_list(config.reject_patterns, new_job->iri->uri))
 				|| (config.reject_regex && regex_match(new_job->iri->uri, config.reject_regex)))
+			new_job->head_first = 1; // enable mime-type check to assure e.g. text/html to be downloaded and parsed
+
+		if (config.exclude_directories && in_directory_pattern_list(config.exclude_directories, new_job->iri->path))
 			new_job->head_first = 1; // enable mime-type check to assure e.g. text/html to be downloaded and parsed
 	}
 
@@ -2926,7 +3005,7 @@ static bool check_mime_list(wget_vector_t *list, const char *mime)
 
 static int G_GNUC_WGET_NONNULL((1)) _prepare_file(wget_http_response_t *resp, const char *fname, int flag,
 		const char *uri, const char *original_url, int ignore_patterns, wget_buffer_t *partial_content,
-		size_t max_partial_content, char **actual_file_name)
+		size_t max_partial_content, char **actual_file_name, const char *path)
 {
 	char *alloced_fname = NULL;
 	int fd, multiple = 0, oflag = flag;
@@ -3034,6 +3113,12 @@ static int G_GNUC_WGET_NONNULL((1)) _prepare_file(wget_http_response_t *resp, co
 				|| (config.reject_regex && regex_match(fname, config.reject_regex)))
 		{
 			debug_printf("not saved '%s' (matches reject pattern)\n", fname);
+			xfree(alloced_fname);
+			return -2;
+		}
+
+		if (config.exclude_directories && in_directory_pattern_list(config.exclude_directories, path)) {
+			debug_printf("not saved '%s' (directory excluded)\n", path);
 			xfree(alloced_fname);
 			return -2;
 		}
@@ -3215,7 +3300,8 @@ static int _get_header(wget_http_response_t *resp, void *context)
 			ctx->job->ignore_patterns,
 			resp->code == 206 ? ctx->body : NULL,
 			ctx->max_memory,
-			&ctx->job->sig_filename);
+			&ctx->job->sig_filename,
+			ctx->job->iri->path);
 		if (ctx->outfd == -1)
 			ret = -1;
 	}

src/wget_options.h
@@ -33,6 +33,9 @@
 
 #include <wget.h>
 
+#define INCLUDED_DIRECTORY_PREFIX '+'
+#define EXCLUDED_DIRECTORY_PREFIX '-'
+
 //types for --https-enforce
 enum {
 	WGET_HTTPS_ENFORCE_NONE,
@@ -103,6 +106,7 @@ struct config {
 		*compression,
 		*config_files,
 		*domains,
+		*exclude_directories,
 		*exclude_domains,
 		*accept_patterns,
 		*reject_patterns,

tests/Makefile.am
@@ -25,7 +25,7 @@ WGET_TESTS = test-wget-1$(EXEEXT) test-c-r$(EXEEXT) test-restrict-ascii$(EXEEXT)
  test-plugin-interception$(EXEEXT) test-plugin-nonexistance$(EXEEXT) test-plugin-failure$(EXEEXT) test--filter-mime-type$(EXEEXT)\
  test--https-enforce-hard1$(EXEEXT) test--https-enforce-hard2$(EXEEXT) test--https-enforce-hard3$(EXEEXT)\
  test--https-enforce-soft1$(EXEEXT) test--https-enforce-soft2$(EXEEXT) test--https-enforce-soft3$(EXEEXT)\
- test-gzip$(EXEEXT) test-compression$(EXEEXT)
+ test-gzip$(EXEEXT) test-compression$(EXEEXT) test-include-and-exclude-directories$(EXEEXT)
 #test--post-file$(EXEEXT) test-E-k$(EXEEXT) test-cookies-http_state$(EXEEXT)
 
 if WITH_GPGME

tests/test-include-and-exclude-directories.c
@@ -1,0 +1,182 @@
+/*
+ * Copyright(c) 2018 Free Software Foundation, Inc.
+ *
+ * This file is part of libwget.
+ *
+ * Libwget is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Libwget is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libwget.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Testing Wget2 --exclude-directories combined with --include-directories
+ */
+
+#include <config.h>
+
+#include <stdlib.h> // exit()
+#include "libtest.h"
+
+int main(void)
+{
+	wget_test_url_t urls[]={
+		{	.name = "/index.html",
+			.code = "200 Dontcare",
+			.body =
+				"<html><head><title>Main Page</title></head><body><p>A link to a" \
+				" <a href=\"http://localhost:{{port}}/firstdir/secondpage.html\">second page</a>." \
+				" Also, a <a href=\"http://localhost:{{port}}/nonexistent\">broken link</a>." \
+				"</p></body></html>",
+			.headers = {
+				"Content-Type: text/html",
+			}
+		},
+		{	.name = "/firstdir/secondpage.html",
+			.code = "200 Dontcare",
+			.body =
+				"<html><head><title>Second Page</title></head><body><p>A link to a" \
+				" <a href=\"http://localhost:{{port}}/seconddir/thirdpage.html\">third page</a>." \
+				" Also, a <a href=\"http://localhost:{{port}}/nonexistent\">broken link</a>." \
+				"</p></body></html>",
+			.headers = {
+				"Content-Type: text/html",
+				"Content-Disposition: attachment; filename=\"filename.html\"",
+			}
+		},
+		{	.name = "/seconddir/thirdpage.html",
+			.code = "200 Dontcare",
+			.body =
+				"<html><head><title>Third Page</title></head><body><p>A link to a" \
+				" <a href=\"http://localhost:{{port}}/firstdir/pub/dummy.txt\">text file</a>." \
+				" Also, a <a href=\"http://localhost:{{port}}/againnonexistent\">broken link</a>." \
+				"</p></body></html>",
+			.headers = {
+				"Content-Type: text/html",
+			}
+		},
+		{	.name = "/firstdir/pub/dummy.txt",
+			.code = "200 Dontcare",
+			.body = "What ever",
+			.headers = {
+				"Content-Type: text/plain",
+			}
+		},
+		{	.name = "/,,/file.txt",
+			.code = "200 Dontcare",
+			.body = "What ever",
+			.headers = {
+				"Content-Type: text/plain",
+			}
+		},
+	};
+
+	// functions won't come back if an error occurs
+	wget_test_start_server(
+		WGET_TEST_RESPONSE_URLS, &urls, countof(urls),
+		WGET_TEST_FEATURE_MHD,
+		0);
+
+	// Download all except /*/pub
+	wget_test(
+		// WGET_TEST_KEEP_TMPFILES, 1,
+		WGET_TEST_OPTIONS, "--exclude-directories=/*/pub -r -nH",
+		WGET_TEST_REQUEST_URL, "index.html",
+		WGET_TEST_EXPECTED_ERROR_CODE, 8,
+		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+			{ urls[0].name + 1, NULL },
+			{ urls[1].name + 1, NULL },
+			{ urls[2].name + 1, NULL },
+			{ NULL } },
+		0);
+
+	// Download only files from / and from /*/pub
+	wget_test(
+		// WGET_TEST_KEEP_TMPFILES, 1,
+		WGET_TEST_OPTIONS, "--include-directories=/,/*/pub -r -nH",
+		WGET_TEST_REQUEST_URL, "index.html",
+		WGET_TEST_EXPECTED_ERROR_CODE, 8,
+		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+			{ urls[0].name + 1, NULL },
+			{ urls[3].name + 1, NULL },
+			{ NULL } },
+		0);
+
+	// Download all except /firstdir, but also download /*/pub
+	wget_test(
+		// WGET_TEST_KEEP_TMPFILES, 1,
+		WGET_TEST_OPTIONS, "--exclude-directories=/firstdir --include-directories=/*/pub -r -nH",
+		WGET_TEST_REQUEST_URL, "index.html",
+		WGET_TEST_EXPECTED_ERROR_CODE, 8,
+		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+			{ urls[0].name + 1, NULL },
+			{ urls[2].name + 1, NULL },
+			{ urls[3].name + 1, NULL },
+			{ NULL } },
+		0);
+
+	// Only download /firstdir, except /firstdir/pub
+	wget_test(
+		// WGET_TEST_KEEP_TMPFILES, 1,
+		WGET_TEST_OPTIONS, "--include-directories=/firstdir --exclude-directories=/firstdir/pub -r -nH",
+		WGET_TEST_REQUEST_URL, "index.html",
+		WGET_TEST_EXPECTED_ERROR_CODE, 8,
+		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+			{ urls[1].name + 1, NULL },
+			{ NULL } },
+		0);
+
+	// Test that /directory is equal to /directory/
+	wget_test(
+		// WGET_TEST_KEEP_TMPFILES, 1,
+		WGET_TEST_OPTIONS, "--include-directories=/firstdir/ --exclude-directories=/firstdir/pub/ -r -nH",
+		WGET_TEST_REQUEST_URL, "index.html",
+		WGET_TEST_EXPECTED_ERROR_CODE, 8,
+		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+			{ urls[1].name + 1, NULL },
+			{ NULL } },
+		0);
+
+	// Only download /first (which doesn't exist, so no download expected at all)
+	wget_test(
+		// WGET_TEST_KEEP_TMPFILES, 1,
+		WGET_TEST_OPTIONS, "--include-directories=/first -r -nH",
+		WGET_TEST_REQUEST_URL, "index.html",
+		WGET_TEST_EXPECTED_ERROR_CODE, 8,
+		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+			{ NULL } },
+		0);
+
+	char *full_path = wget_aprintf("localhost%s", urls[3].name);
+	// Download /*/pub with hostname directory (we omit -nH)
+	wget_test(
+		// WGET_TEST_KEEP_TMPFILES, 1,
+		WGET_TEST_OPTIONS, "--include-directories=/*/pub -r",
+		WGET_TEST_REQUEST_URL, "index.html",
+		WGET_TEST_EXPECTED_ERROR_CODE, 8,
+		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+			{ full_path, NULL },
+			{ NULL } },
+		0);
+	wget_xfree(full_path);
+
+	// Download only /,\,/file.txt
+	wget_test(
+		// WGET_TEST_KEEP_TMPFILES, 1,
+		WGET_TEST_OPTIONS, "--include-directories='/\\,\\,'",
+		WGET_TEST_REQUEST_URL, ",,/file.txt",
+		WGET_TEST_EXPECTED_ERROR_CODE, 0,
+		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+			{ strrchr(urls[4].name, '/') + 1, NULL },
+			{ NULL } },
+		0);
+
+	exit(0);
+}

commit f6e762181df35bb081d8e776df6cbb6af4bd5939

Fix buffer overflow in -X/-I parsing
* src/options.c (set_char_prefix): Fix scratch buffer size
* fuzz/wget_options_fuzzer.in/: Add reproducer

This bug was introduced yesterday by commit
756e470de942ec830a9a424a2340b7e986e9fe7c

Found by oss-fuzz (issue 10159).

fuzz/wget_options_fuzzer.in/clusterfuzz-testcase-minimized-wget_options_fuzzer-5704034783068160
@@ -1,0 +1,1 @@
+includedirectories=,

src/options.c
@@ -469,7 +469,7 @@ static char *set_char_prefix(const char *val, char prefix)
 {
 	if (val && *val) {
 		// we just need a scratch buffer, no need for optimal size calculation
-		char *prefixed_val = wget_malloc(strlen(val) * 2 + 1), *dst = prefixed_val;
+		char *prefixed_val = wget_malloc(strlen(val) * 3 + 1), *dst = prefixed_val;
 
 		*dst++ = prefix;
 		for (const char *src = val; *src; src++) {