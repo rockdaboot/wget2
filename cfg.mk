e2bf7c1c181047da46aa984a74c4fb4273b72dc9
Tim Rühsen <tim.ruehsen@gmx.de>
2019-05-14 17:27:50 +0200

    * cfg.mk: Fix syntax-check (bindtextdomain)

--- cfg.mk
+++ cfg.mk
@@ -13,7 +13,7 @@ local-checks-to-skip = \
   sc_prohibit_strcmp
 
 # Explicit syntax-check exceptions.
-exclude_file_name_regexp--sc_bindtextdomain = ^(tests|unit-tests|examples|fuzz)/.*\.c|^libwget/test_linking\.c$$
+exclude_file_name_regexp--sc_bindtextdomain = ^(tests|unit-tests|examples|fuzz)/.*\.c|^libwget/test_linking.*\.c$$
 exclude_file_name_regexp--sc_po_check = ^examples/|tests/.*\.c$$
 exclude_file_name_regexp--sc_prohibit_magic_number_exit = tests/.*\.c$$
 exclude_file_name_regexp--sc_trailing_blank = docs/DoxygenLayout\.xml|docs/libwget\.doxy\.in|contrib/assignment_template\.txt$$
