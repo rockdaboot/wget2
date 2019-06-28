1e6db2100f18fb1a8782c15e02e0b6ab18fdcae5
Tim Rühsen <tim.ruehsen@gmx.de>
2019-06-28 13:23:00 +0200

    Generate library libwget_iri
    * libwget/Makefile.am: Add libwget_iri.la
    * libwget/test_linking_iri.c: New file

---
+++ libwget/test_linking_iri.c
@@ -0,0 +1,11 @@
+// Just a test for static linking.
+// We call one function from each object file in libwget_thread
+// Unresolved references should come up on linking.
+
+#include <config.h>
+#include <wget.h>
+
+int main(void)
+{
+	wget_iri_parse(NULL, NULL);
+}
