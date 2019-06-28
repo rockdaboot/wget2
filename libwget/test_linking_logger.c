7d7c4fe637239feada08b02432d5494c7589209a
Tim Rühsen <tim.ruehsen@gmx.de>
2019-06-28 15:33:56 +0200

    Generate library libwget_logger
    * libwget/Makefile.am: Add libwget_logger.la
    * libwget/test_linking_logger.c: New file

---
+++ libwget/test_linking_logger.c
@@ -0,0 +1,12 @@
+// Just a test for static linking.
+// We call one function from each object file in libwget_thread
+// Unresolved references should come up on linking.
+
+#include <config.h>
+#include <wget.h>
+
+int main(void)
+{
+	wget_logger_set_func(NULL, NULL);
+	return 0;
+}
