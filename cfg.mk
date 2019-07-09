084b7d10543a11cd521de1ecbb7e543b280a31f9
Tim Rühsen <tim.ruehsen@gmx.de>
2019-07-09 10:47:06 +0200

    * cfg.mk: Add new syntax-check rule 'sc_gettext_printf'

--- cfg.mk
+++ cfg.mk
@@ -26,6 +26,7 @@ exclude_file_name_regexp--sc_prohibit_sprintf = benchmarks/benches/convert\.gp$$
 exclude_file_name_regexp--sc_prohibit_printf = ^(unit-tests/(test\.c|buffer_printf_perf\.c)|examples/.*\.c|libwget/strlcpy\.c)$$
 exclude_file_name_regexp--sc_prohibit_free = ^(cfg\.mk|fuzz/.*\.c|unit-tests/.*\.c)$$
 exclude_file_name_regexp--sc_prohibit_alloc = ^(fuzz/.*\.c)$$
+exclude_file_name_regexp--sc_gettext_printf = ^(fuzz|tests|unit-tests|examples)/.*\.c|.*\.h|libwget/test_linking.*\.c$$
 
 sc_prohibit_sprintf:
         @prohibit='\<sprintf *\(' \
@@ -50,4 +51,10 @@ sc_prohibit_alloc:
 sc_prohibit_gettext_debug:
 	@prohibit='\<(wget_|)debug_printf *\( *_ *\(' \
 	halt='do not translate debug strings' \
+	  $(_sc_search_regexp)
+
+sc_gettext_printf:
+	@prohibit='\<(wget_|)(info|error)_printf *\( *[^_]' \
+	exclude='(//.*\<(wget_|)(info|error)_printf|\<wget_(info|error)_printf\(const |no translation)' \
+	halt='use _() to translate info and error strings' \
 	  $(_sc_search_regexp)
