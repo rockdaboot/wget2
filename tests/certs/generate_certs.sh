bac04ab8ea6980f76b33898c3a7f3dc91cd80b06
Kumar Mallikarjuna <kumarmallikarjuna1@gmail.com>
2019-08-24 22:46:25 +0530

    Regenerating certificates for openssl compatibility
    * tests/certs/generate_certs.sh: Script for certificate generation
    * tests/certs/x509-ca-cert.pem: CA certificate
    * tests/certs/x509-ca-key.pem: CA key
    * tests/certs/x509-server-cert.pem: Server certificate
    * tests/certs/x509-server-key.pem: Server key
    * tests/certs/x509-server-crl.pem: CRL for the server certificate
    * tests/certs/ca-template.txt: Template for CA Certificate
    * tests/certs/server-template.txt: Template for Server Certificate

---
+++ tests/certs/generate_certs.sh
@@ -0,0 +1,14 @@
+# Generate CA private key
+certtool --generate-privkey --outfile x509-ca-key.pem --rsa
+
+# Generate CA certificate
+certtool --generate-self-signed --load-privkey x509-ca-key.pem --template ca-template.txt --outfile x509-ca-cert.pem
+
+# Generate server private key
+certtool --generate-privkey --outfile x509-server-key.pem --rsa
+
+# Generate server certificate
+certtool --generate-certificate --load-privkey x509-server-key.pem --template server-template.txt --outfile x509-server-cert.pem --load-ca-certificate x509-ca-cert.pem --load-ca-privkey x509-ca-key.pem
+
+# Generate CRL for the server certificate
+certtool --generate-crl --load-ca-privkey x509-ca-key.pem --load-ca-certificate x509-ca-cert.pem --load-certificate x509-server-cert.pem --outfile x509-server-crl.pem
