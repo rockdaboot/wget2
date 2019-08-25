d8323da63dfc20adace3981180280e93ef64f099
Tim Rühsen <tim.ruehsen@gmx.de>
2019-08-25 15:46:19 +0200

    Update test suite certs and build script
    * tests/certs/revoked-template.txt: New file
    * tests/certs/ca-template.txt: Update
    * tests/certs/generate_certs.sh: Use revoked-template.txt
    * tests/certs/server-template.txt: Update
    * tests/certs/*.pem: Update, generated by generate_certs.sh

--- tests/certs/generate_certs.sh
+++ tests/certs/generate_certs.sh
@@ -11,4 +11,4 @@ certtool --generate-privkey --outfile x509-server-key.pem --rsa
 certtool --generate-certificate --load-privkey x509-server-key.pem --template server-template.txt --outfile x509-server-cert.pem --load-ca-certificate x509-ca-cert.pem --load-ca-privkey x509-ca-key.pem
 
 # Generate CRL for the server certificate
-certtool --generate-crl --load-ca-privkey x509-ca-key.pem --load-ca-certificate x509-ca-cert.pem --load-certificate x509-server-cert.pem --outfile x509-server-crl.pem
+certtool --generate-crl --load-ca-privkey x509-ca-key.pem --load-ca-certificate x509-ca-cert.pem --load-certificate x509-server-cert.pem --outfile x509-server-crl.pem --template revoked-template.txt
