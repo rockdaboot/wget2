5d7253357034cc196bca17e7a0bb833b6900589e
Darshit Shah <darnir@gnu.org>
2019-09-13 23:46:33 +0200

    Fix license of all tests
    All the tests in tests/ are part of Wget2 the binary, not libwget. Fix
    the license and copyright information accordingly.
    
    * tests/libtest.c: libwget -> Wget. LGPLv3+ -> GPLv3+
    * tests/test--accept.c: Same
    * tests/test--filter-mime-type.c: Same
    * tests/test--follow-tags.c: Same
    * tests/test--https-enforce-hard1.c: Same
    * tests/test--https-enforce-hard2.c: Same
    * tests/test--https-enforce-hard3.c: Same
    * tests/test--https-enforce-soft1.c: Same
    * tests/test--https-enforce-soft2.c: Same
    * tests/test--https-enforce-soft3.c: Same
    * tests/test--page-requisites.c: Same
    * tests/test--post-file.c: Same
    * tests/test--save-content-on.c: Same
    * tests/test--spider-r.c: Same
    * tests/test-E-k.c: Same
    * tests/test-auth-basic.c: Same
    * tests/test-auth-digest.c: Same
    * tests/test-bad-chunk.c: Same
    * tests/test-base.c: Same
    * tests/test-c-r.c: Same
    * tests/test-chunked.c: Same
    * tests/test-compression.c: Same
    * tests/test-cut-dirs.c: Same
    * tests/test-cut-get-vars.c: Same
    * tests/test-directory-clash.c: Same
    * tests/test-ftp.c: Same
    * tests/test-ftps.c: Same
    * tests/test-gpg-bad.c: Same
    * tests/test-gpg-invalid.c: Same
    * tests/test-gpg-missing.c: Same
    * tests/test-gpg-save-failed.c: Same
    * tests/test-gpg-styles.c: Same
    * tests/test-gpg-valid.c: Same
    * tests/test-gpg-verify-no-file.c: Same
    * tests/test-gzip.c: Same
    * tests/test-i-http.c: Same
    * tests/test-i-https.c: Same
    * tests/test-idn-cmd.c: Same
    * tests/test-idn-meta.c: Same
    * tests/test-idn-robots.c: Same
    * tests/test-include-and-exclude-directories.c: Same
    * tests/test-iri-disabled.c: Same
    * tests/test-iri-forced-remote.c: Same
    * tests/test-iri-list.c: Same
    * tests/test-iri-percent.c: Same
    * tests/test-iri-subdir.c: Same
    * tests/test-iri.c: Same
    * tests/test-k.c: Same
    * tests/test-limit-rate-http2.c: Same
    * tests/test-limit-rate.c: Same
    * tests/test-meta-robots.c: Same
    * tests/test-metalink.c: Same
    * tests/test-np.c: Same
    * tests/test-ocsp-server.c: Same
    * tests/test-ocsp-stap.c: Same
    * tests/test-p-nc.c: Same
    * tests/test-parse-css.c: Same
    * tests/test-parse-html-css.c: Same
    * tests/test-parse-rss.c: Same
    * tests/test-plugin-dummy.c: Same
    * tests/test-plugin-failure.c: Same
    * tests/test-plugin-interception.c: Same
    * tests/test-plugin-nonexistance.c: Same
    * tests/test-plugin.c: Same
    * tests/test-post-handhshake-auth.c: Same
    * tests/test-redirection.c: Same
    * tests/test-restrict-ascii.c: Same
    * tests/test-robots.c: Same
    * tests/test-stats-dns.c: Same
    * tests/test-stats-ocsp.c: Same
    * tests/test-stats-server.c: Same
    * tests/test-stats-site.c: Same
    * tests/test-stats-tls.c: Same
    * tests/test-timestamping.c: Same
    * tests/test-unlink.c: Same
    * tests/test-wget-1.c: Same

--- tests/test-directory-clash.c
+++ tests/test-directory-clash.c
@@ -2,20 +2,20 @@
  * Copyright (c) 2014 Tim Ruehsen
  * Copyright (c) 2015-2019 Free Software Foundation, Inc.
  *
- * This file is part of libwget.
+ * This file is part of Wget
  *
- * Libwget is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * Wget is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Libwget is distributed in the hope that it will be useful,
+ * Wget is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with libwget.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with Wget  If not, see <https://www.gnu.org/licenses/>.
  *
  *
  * Testing file/directory ambiguity
