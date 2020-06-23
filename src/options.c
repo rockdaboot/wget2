rockdaboot/wget2

commit fa3c71c854b37355494920c4b2d04bfdf9342703

Improve handling of initialization files
* docs/wget2.md: Add section on initialization files and the order in
which they are read.
* src/options.c(struct config config): Set system_config to the default
value of "SYSCONFDIR/wget2rc"
(struct optionw): --config now maps to config.user_config and update the
value of the default path
(read_config): Read the configuration for system and user configs, if
they're set
(get_config_files): New function that sets the values for
config.system_config and config.user_config. For the system config it
picks either the file in `$SYSTEM_WGET2RC` or at
`/usr/local/etc/wget2rc`. For the user config, it picks the first file from
`--config`, `$WGET2RC`, `$XDG_CONFIG_HOME/wget/wget2rc`,
`~/.config/wget/wget2rc` or `$HOME/.wget2rc`.
(init): Remove the code for finding all the config files and move it to
`get_config_file`.
(deinit): Free config.user_config and config.system_config. Remove
config.config_files.
* src/wget_options.h(struct config): New char * user_config and
system_config. Remove config.config_files

src/options.c
@@ -1116,6 +1116,7 @@ struct config config = {
 	.hsts = 1,
 	.hsts_preload = 1,
 	.hpkp = 1,
+	.system_config = SYSCONFDIR"wget2rc",
 
 #if defined WITH_LIBNGHTTP2
 	.http2 = 1,
@@ -1260,9 +1261,9 @@ static const struct optionw options[] = {
 		   "no-compression means no Accept-Encoding\n"
 		}
 	},
-	{ "config", &config.config_files, parse_filenames, 1, 0,
+	{ "config", &config.user_config, parse_filename, 1, 0,
 		SECTION_STARTUP,
-		{  "List of config files. (default: ~/.wget2rc)\n"
+		{  "Path to initialization file (default: ~/.config/wget/wget2rc)\n"
 		}
 	}, // for backward compatibility only
 	{ "connect-timeout", &config.connect_timeout, parse_timeout, 1, 0,
@@ -2580,14 +2581,15 @@ static int G_GNUC_WGET_NONNULL((1)) _read_config(const char *cfgfile, int expand
 	return ret;
 }
 
-static int read_config(void)
+static bool read_config(void)
 {
-	int ret = 0;
+	int ret = true;
 
-	for (int it = 0; it < wget_vector_size(config.config_files) && ret == 0; it++) {
-		const char *cfgfile = wget_vector_get(config.config_files, it);
-		ret = _read_config(cfgfile, 1);
-	}
+	if (config.system_config)
+		ret = _read_config(config.system_config, 1);
+
+	if (config.user_config)
+		ret &= _read_config(config.user_config, 1);
 
 	return ret;
 }
@@ -2689,7 +2691,6 @@ static int _no_memory(void)
 
 
 // Return the user's home directory (strdup-ed), or NULL if none is found.
-// TODO: Read the XDG Base Directory variables first
 static char *get_home_dir(void)
 {
 	char *home;
@@ -2849,6 +2850,48 @@ static int _preload_dns_cache(const char *fname)
 	return 0;
 }
 
+static inline void G_GNUC_WGET_NONNULL_ALL get_config_files(const char *home_dir)
+{
+	const char *env;
+
+	// First add the Global Wget2rc file.
+	if ((env = getenv ("SYSTEM_WGET2RC")) && *env) {
+		config.system_config = wget_strdup(env);
+	} else {
+		if (config.system_config && access(config.system_config, R_OK) != 0)
+			config.system_config = NULL;
+	}
+
+	if (!config.user_config && (env = getenv("WGET2RC")) && *env) {
+		// If the WGET2RC variable is set, then load that file
+		config.user_config = wget_strdup(env);
+	}
+
+	if (!config.user_config && (env = getenv("XDG_DATA_HOME")) && *env) {
+		const char *path = wget_aprintf("%s/wget/wget2rc", env);
+		if (access(path, R_OK) == 0)
+			config.user_config = wget_strdup(path);
+		else
+			xfree(path);
+	}
+
+	if (!config.user_config) {
+		const char *path = wget_aprintf("%s/.config/wget/wget2rc", home_dir);
+		if (access(path, R_OK) == 0)
+			config.user_config = wget_strdup(path);
+		else
+			xfree(path);
+	}
+
+	if (!config.user_config) {
+		const char *path = wget_aprintf("%s/.wget2rc", home_dir);
+		if (access(path, R_OK) == 0)
+			config.user_config = wget_strdup(path);
+		else
+			xfree(path);
+	}
+}
+
 // read config, parse CLI options, check values, set module options
 // and return the number of arguments consumed
 
@@ -2880,29 +2923,7 @@ int init(int argc, const char **argv)
 	config.ca_directory = wget_strdup(config.ca_directory);
 	config.default_page = wget_strdup(config.default_page);
 
-	// create list of default config file names
-	const char *env;
-	config.config_files = wget_vector_create(8, NULL);
-
-	// First add the Global Wget2rc file.
-	if ((env = getenv ("SYSTEM_WGET2RC")) && *env) {
-		wget_vector_add_str(config.config_files, env);
-	} else {
-		const char *cfgfile = SYSCONFDIR"wget2rc";
-		if (access(cfgfile, R_OK) == 0)
-			wget_vector_add_str(config.config_files, cfgfile);
-	}
-
-	if ((env = getenv ("WGET2RC")) && *env)
-		wget_vector_add_str(config.config_files, env);
-	else {
-		// we don't want to complain about missing home .wget2rc
-		const char *cfgfile = wget_aprintf("%s/.wget2rc", home_dir);
-		if (access(cfgfile, R_OK) == 0)
-			wget_vector_insert_noalloc(config.config_files, cfgfile, 0);
-		else
-			xfree(cfgfile);
-	}
+	get_config_files(home_dir);
 
 	log_init();
 
@@ -3324,6 +3345,8 @@ void deinit(void)
 	xfree(config.stats_server);
 	xfree(config.stats_site);
 	xfree(config.stats_tls);
+	xfree(config.user_config);
+	xfree(config.system_config);
 
 	wget_iri_free(&config.base);
 
@@ -3338,7 +3361,6 @@ void deinit(void)
 	wget_vector_free(&config.accept_patterns);
 	wget_vector_free(&config.reject_patterns);
 	wget_vector_free(&config.headers);
-	wget_vector_free(&config.config_files);
 	wget_vector_free(&config.default_challenges);
 	wget_vector_free(&config.compression);
 #ifdef WITH_GPGME

src/wget_options.h
@@ -99,6 +99,8 @@ struct config {
 		*stats_server,
 		*stats_site,
 		*stats_tls,
+		*system_config,
+		*user_config,
 		*hsts_file,
 		*hsts_preload_file,
 		*hpkp_file,
@@ -109,7 +111,6 @@ struct config {
 		*dns_cache_preload;
 	wget_vector_t
 		*compression,
-		*config_files,
 		*domains,
 		*exclude_directories,
 		*exclude_domains,

commit 92aed8bc2c9bd107f6b1d8835f5d1eab4f9b9735

* src/options.c: Fix crash introduced in fa3c71c85

src/options.c
@@ -3040,9 +3040,9 @@ static void WGET_GCC_NONNULL_ALL get_config_files(const char *config_home, const
 	// First add the Global Wget2rc file.
 	if ((env = getenv ("SYSTEM_WGET2RC")) && *env) {
 		config.system_config = wget_strdup(env);
-	} else {
-		if (config.system_config && access(config.system_config, R_OK) != 0)
-			config.system_config = NULL;
+	} else if (config.system_config) {
+		if (access(config.system_config, R_OK) != 0)
+			xfree(config.system_config);
 	}
 
 	if ((env = getenv("WGET2RC")) && *env) {
@@ -3242,6 +3242,7 @@ int init(int argc, const char **argv)
 	config.secure_protocol = wget_strdup(config.secure_protocol);
 	config.ca_directory = wget_strdup(config.ca_directory);
 	config.default_page = wget_strdup(config.default_page);
+	config.system_config = wget_strdup(config.system_config);
 
 	log_init();
 