d735738d734a1202fadd6355721b0a1d794c6774
Mitch Capper <mitch.capper@gmail.com>
2023-08-14 04:23:59 -0700

    Don't enable debug logging with wolfssl unless defined prior to including the options file
    * libwget/ssl_wolfssl.c: Test for DEBUG_WOLFSSL

--- libwget/ssl_wolfssl.c
+++ libwget/ssl_wolfssl.c
@@ -45,7 +45,12 @@
 #  define SOCKET_TO_FD(x) (x)
 #endif
 
+#ifndef DEBUG_WOLFSSL
 #include <wolfssl/options.h>
+#undef DEBUG_WOLFSSL
+#else
+#include <wolfssl/options.h>
+#endif
 #include <wolfssl/ssl.h>
 
 #include <wget.h>
