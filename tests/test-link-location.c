rockdaboot/wget2

commit 400713caebb51e17046c7d884e08729a27cfa505

Fix redirect regression
* src/wget.c (process_response_header): Follow location header.

Fixes https://gitlab.com/gnuwget/wget2/-/issues/689
Reported-by: https://gitlab.com/omos

src/wget.c
@@ -1907,6 +1907,16 @@ static int process_response_header(wget_http_response *resp)
 
 		wget_cookie_normalize_cookies(job->iri, resp->cookies);
 		wget_cookie_store_cookies(config.cookie_db, resp->cookies);
+
+		wget_buffer uri_buf;
+		char uri_sbuf[1024];
+		wget_buffer_init(&uri_buf, uri_sbuf, sizeof(uri_sbuf));
+
+		wget_iri_relative_to_abs(iri, resp->location, (size_t) -1, &uri_buf);
+		if (uri_buf.length)
+			queue_url_from_remote(job, "utf-8", uri_buf.data, URL_FLG_REDIRECTION, NULL);
+
+		wget_buffer_deinit(&uri_buf);
 	}
 
 	return 0;

commit 0390345ceb80dbc9a2854576cdb2ad0ef72c42b5

Fix redirect/mirror regression from 400713ca (#698)
* src/wget.c (process_response_header): Delegate link handling to process_response().
* tests/Makefile.am: Add test-link-location.c.
* tests/test-link-location.c: New test file.

src/wget.c
@@ -1923,15 +1923,19 @@ static int process_response_header(wget_http_response *resp)
 		wget_cookie_normalize_cookies(job->iri, resp->cookies);
 		wget_cookie_store_cookies(config.cookie_db, resp->cookies);
 
-		wget_buffer uri_buf;
-		char uri_sbuf[1024];
-		wget_buffer_init(&uri_buf, uri_sbuf, sizeof(uri_sbuf));
+		// If Link headers are present, process_response() will handle the redirect
+		// with better logic for choosing the best mirror/metalink.
+		if (!wget_vector_size(resp->links)) {
+			wget_buffer uri_buf;
+			char uri_sbuf[1024];
+			wget_buffer_init(&uri_buf, uri_sbuf, sizeof(uri_sbuf));
 
-		wget_iri_relative_to_abs(iri, resp->location, (size_t) -1, &uri_buf);
-		if (uri_buf.length)
-			queue_url_from_remote(job, "utf-8", uri_buf.data, URL_FLG_REDIRECTION, NULL);
+			wget_iri_relative_to_abs(iri, resp->location, (size_t) -1, &uri_buf);
+			if (uri_buf.length)
+				queue_url_from_remote(job, "utf-8", uri_buf.data, URL_FLG_REDIRECTION, NULL);
 
-		wget_buffer_deinit(&uri_buf);
+			wget_buffer_deinit(&uri_buf);
+		}
 	}
 
 	return 0;

tests/Makefile.am
@@ -44,7 +44,7 @@ WGET_TESTS = test-wget-1$(EXEEXT) test-c-r$(EXEEXT) test-restrict-ascii$(EXEEXT)
  test-np$(EXEEXT) test-spider-r$(EXEEXT) test-meta-robots$(EXEEXT) test-idn-robots$(EXEEXT) test-idn-meta$(EXEEXT)\
  test-idn-cmd$(EXEEXT) test-iri$(EXEEXT) test-iri-percent$(EXEEXT) test-iri-list$(EXEEXT) test-iri-forced-remote$(EXEEXT)\
  test-auth-basic$(EXEEXT) test-parse-rss$(EXEEXT) test-page-requisites$(EXEEXT) test-p-nc$(EXEEXT)\
- test-accept$(EXEEXT) test-k$(EXEEXT) test-k-percent-encoding$(EXEEXT) test-follow-tags$(EXEEXT) test-directory-clash$(EXEEXT) test-redirection$(EXEEXT)\
+ test-accept$(EXEEXT) test-k$(EXEEXT) test-k-percent-encoding$(EXEEXT) test-follow-tags$(EXEEXT) test-directory-clash$(EXEEXT) test-redirection$(EXEEXT) test-link-location$(EXEEXT)\
  test-base$(EXEEXT) test-metalink$(EXEEXT) test-robots$(EXEEXT) test-robots-off$(EXEEXT) test-parse-css$(EXEEXT) test-bad-chunk$(EXEEXT)\
  test-iri-subdir$(EXEEXT) test-chunked$(EXEEXT) test-cut-dirs$(EXEEXT) test-cut-get-vars$(EXEEXT) test-parse-html-css$(EXEEXT)\
  test-auth-digest$(EXEEXT) test-stats-dns$(EXEEXT) test-stats-tls$(EXEEXT) test-stats-site$(EXEEXT) test-stats-ocsp$(EXEEXT)\

tests/test-link-location.c
@@ -1,0 +1,82 @@
+/*
+ * Copyright (c) 2025 Free Software Foundation, Inc.
+ *
+ * This file is part of Wget
+ *
+ * Wget is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wget is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Wget  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Testing redirects with both Link and Location headers (e.g., MirrorBrain)
+ *
+ * This test reproduces the issue where a redirect with both Link and
+ * Location headers causes the file to be downloaded multiple times.
+ */
+
+#include <config.h>
+
+#include <stdlib.h> // exit()
+#include "libtest.h"
+
+int main(void)
+{
+	wget_test_url_t urls[]={
+		// Simulates a MirrorBrain-style redirect with both Link and Location headers
+		// The Location header points to mirror2, but Link headers say mirror1 is best
+		// This reproduces the bug where both URLs get queued and downloaded
+		{	.name = "/file.zip",
+			.code = "302 Found",
+			.headers = {
+				"Content-Type: text/html",
+				"Link: <http://localhost:{{port}}/mirror1/file.zip>; rel=duplicate; pri=1",
+				"Link: <http://localhost:{{port}}/mirror2/file.zip>; rel=duplicate; pri=2",
+				"Location: http://localhost:{{port}}/mirror2/file.zip",
+			}
+		},
+		// First mirror (highest priority in Link headers)
+		{	.name = "/mirror1/file.zip",
+			.code = "200 OK",
+			.body = "file contents",
+			.headers = {
+				"Content-Type: application/zip",
+			}
+		},
+		// Second mirror (Location header points here)
+		{	.name = "/mirror2/file.zip",
+			.code = "200 OK",
+			.body = "file contents",
+			.headers = {
+				"Content-Type: application/zip",
+			}
+		},
+	};
+
+	// functions won't come back if an error occurs
+	wget_test_start_server(
+		WGET_TEST_RESPONSE_URLS, &urls, countof(urls),
+		WGET_TEST_FEATURE_MHD,
+		0);
+
+	// Test: Download file with both Link and Location headers
+	// Expected: Only ONE file should be downloaded (not multiple)
+	wget_test(
+//		WGET_TEST_KEEP_TMPFILES, 1,
+		WGET_TEST_REQUEST_URL, "file.zip",
+		WGET_TEST_EXPECTED_ERROR_CODE, 0,
+		WGET_TEST_EXPECTED_FILES, &(wget_test_file_t []) {
+			{ "file.zip", urls[1].body },  // Should download only once
+			{	NULL } },
+		0);
+
+	exit(EXIT_SUCCESS);
+}